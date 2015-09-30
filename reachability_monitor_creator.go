cee29cccfee4402034437205e0476932f4882199
Thomas Voß <thomas.voss.bochum@gmail.com>
2015-09-30 08:30:34 +0200

    remove further files

--- reachability_monitor_creator.go
+++
@@ -1,8 +0,0 @@
-package main
-
-// ReachbilityMonitorCreator abstracts creation of ReachabilityMonitor instances.
-type ReachabilityMonitorCreator interface {
-	// Create returns a new ReachabilityMonitor instance monitoring reachability
-	// of the given host or IP. Returns an error if setup of the monitor fails.
-	Create(hostOrIp string) (ReachabilityMonitor, error)
-}
