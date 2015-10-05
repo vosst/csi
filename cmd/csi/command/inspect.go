6567d269d83e698166dbf4e06889343953e354be
Thomas Voß <thomas.voss.bochum@gmail.com>
2015-10-05 13:57:05 +0200

    make system inspection a subcommand of inspect

--- cmd/csi/command/inspect.go
+++ cmd/csi/command/inspect.go
@@ -1,27 +1,13 @@
 package command
 
 import (
-	"encoding/json"
-	"fmt"
-
 	"github.com/codegangsta/cli"
-	"github.com/vosst/csi"
+	"github.com/vosst/csi/cmd/csi/command/inspect"
 )
-
-func actionInspect(context *cli.Context) {
-	si := csi.SystemInspector{}
-	sysInfo, _ := si.Inspect()
-
-	if b, err := json.MarshalIndent(sysInfo, "", "  "); err != nil {
-		fmt.Fprintf(context.App.Writer, "Failed to query system information")
-	} else {
-		fmt.Fprintf(context.App.Writer, "%s\n", b)
-	}
-}
 
 // Inspect collects information about the entire system or a specific subsystem.
 var Inspect = cli.Command{
-	Name:   "inspect",
-	Usage:  "inspects and collects information about the (sub)system",
-	Action: actionInspect,
+	Name:        "inspect",
+	Usage:       "inspects and collects information about the system and specific characteristics",
+	Subcommands: []cli.Command{inspect.System},
 }
