dc60525af06aa28ebabecfe18b3b483a614f377d
Thomas Voß <thomas.voss.bochum@gmail.com>
2015-10-14 14:56:03 +0200

    Adjust umask setup when creating files and directories

--- cmd/csi/command/dump.go
+++ cmd/csi/command/dump.go
@@ -20,6 +20,7 @@ var (
 	dumpLogger, _ = syslog.NewLogger(syslog.LOG_INFO|syslog.LOG_USER, log.LstdFlags)
 
 	dumpFlagVerbose  = cli.BoolFlag{"verbose", "enables verbose output to syslog for debugging purposes", ""}
+	dumpFlagCompress = cli.BoolFlag{"compress", "compress the core dump with snappy", ""}
 	dumpFlagCrashDir = cli.StringFlag{"crash-dir", "/var/crash", "destination directory for crash reports", ""}
 	dumpFlagPid      = cli.IntFlag{"pid", -1, "pid of the crashed process", ""}
 	dumpFlagUid      = cli.IntFlag{"uid", -1, "real UID of dumped process", ""}
@@ -32,23 +33,22 @@ var (
 )
 
 func actionDump(c *cli.Context) {
-	// Make sure that we are using a predicable umask
-	oldMask := syscall.Umask(0000)
-	defer syscall.Umask(oldMask)
-
 	verbose := c.Bool(dumpFlagVerbose.Name)
+	compress := c.Bool(dumpFlagCompress.Name)
 	cd := c.String(dumpFlagCrashDir.Name)
 	pid := c.Int(dumpFlagPid.Name)
-	// uid := c.Int(dumpFlagUid.Name)
-	// gid := c.Int(dumpFlagGid.Name)
-	// sig := c.Int(dumpFlagSig.Name)
 	when := time.Unix(int64(c.Int(dumpFlagTime.Name)), 0)
-	// size := c.String(dumpFlagSize.Name)
-	// host := c.String(dumpFlagHost.Name)
 	exe := c.String(dumpFlagExe.Name)
 
+	// Make sure that we are using a sensible umask.
+	oldMask := syscall.Umask(0000)
+	if verbose {
+		dumpLogger.Printf("Old umask %#o", oldMask)
+	}
+	defer syscall.Umask(oldMask)
+
 	cd = filepath.Join(cd, exe, when.Format(time.RFC3339), fmt.Sprint(pid))
-	if err := os.MkdirAll(cd, 0777); err != nil {
+	if err := os.MkdirAll(cd, 0755); err != nil {
 		fmt.Fprintf(c.App.Writer, "Failed to create crash directory %s [%s]\n", cd, err)
 	}
 
@@ -94,19 +94,25 @@ func actionDump(c *cli.Context) {
 		defer f.Close()
 		// TODO(tvoss): Investigate into syscall.Sendfile and figure out a way
 		// to avoid copying of data to userspace.
+		var dest io.Writer = f
+		if compress {
+			dest = snappy.NewWriter(f)
+		}
+
 		start := time.Now()
-		n, _ := io.Copy(snappy.NewWriter(f), os.Stdin)
+		n, _ := io.Copy(dest, os.Stdin)
 		elapsed := time.Since(start)
 
 		if verbose {
-			dumpLogger.Printf("Wrote %d bytes of compressed core to %s in %f seconds", n, df, elapsed.Seconds())
+			dumpLogger.Printf("Wrote %d bytes of core dump to %s in %f seconds", n, df, elapsed.Seconds())
 		}
 	}
 }
 
 var Dump = cli.Command{
-	Name:   "dump",
-	Usage:  "dumps information about a crashed process",
-	Action: actionDump,
-	Flags:  []cli.Flag{dumpFlagVerbose, dumpFlagCrashDir, dumpFlagPid, dumpFlagUid, dumpFlagGid, dumpFlagSig, dumpFlagTime, dumpFlagHost, dumpFlagExe, dumpFlagSize},
+	Name:        "dump",
+	Usage:       "dumps information about a crashed process",
+	Description: `Usually used as the default core dump handler. Install in your system with 'csi install' (requires elevated privileges).`,
+	Action:      actionDump,
+	Flags:       []cli.Flag{dumpFlagVerbose, dumpFlagCrashDir, dumpFlagPid, dumpFlagUid, dumpFlagGid, dumpFlagSig, dumpFlagTime, dumpFlagHost, dumpFlagExe, dumpFlagSize},
 }
